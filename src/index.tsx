13278561b1cafea8e0e04398df7b2bd272f03d63
Leon Boshuizen <llc.boshuizen@gmail.com>
2021-09-28 21:29:25 +0200

    home screen

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { store } from "./store";
 import { RuleEditView } from "./views/editrules";
 import { ScreeningView } from "./views/screening";
 
+import { Button, Card } from "antd";
+
 import "antd/dist/antd.css";
 import "./assets/styles.css";
 
@@ -14,10 +16,58 @@ import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
+const splash = require("./assets/splash.png");
+
 const Home: React.FunctionComponent<{}> = () => {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div
+      style={{
+        position: "absolute",
+        backgroundColor: "#3A8EDBE5",
+        height: "100vh",
+      }}
+    >
+      <Card
+        style={{
+          marginTop: "250px",
+          borderRadius: "20px",
+          width: "80%",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <div style={{ float: "left", maxWidth: "30%" }}>
+          <img src={splash} />
+        </div>
+        <div style={{ float: "left", maxWidth: "50%", marginLeft: "200px" }}>
+          <p style={{ fontWeight: "bolder", fontSize: "25px" }}>
+            The importance of a more relevant FRISS score tailored to your
+            business
+          </p>
+          <p style={{ fontSize: "17px" }}>
+            A good rule set creates an accurate FRISS score. An accurate FRISS
+            score will let you pay legitimate claims faster and detect insurance
+            fraud before claims are paid
+          </p>
+          <div style={{ float: "right", marginTop: "100px" }}>
+            <Button
+              type="primary"
+              style={{
+                width: "210px",
+                height: "70px",
+                borderRadius: "6px",
+                fontSize: "24px",
+              }}
+              onClick={() =>
+                (window.location.href =
+                  "/ruledit/8bbbba9a-cdef-4012-b456-717c2cd8143b")
+              }
+            >
+              Get Started
+            </Button>
+          </div>
+        </div>
+      </Card>
     </div>
   );
 };
